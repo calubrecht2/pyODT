2320fb1bdab6bf86391914be5a82aa64afcbd9c2
Chad Lubrecht <chad@calubrecht.us>
2013-06-21 19:15:25 -0400

    Do it easier

--- ODT.py
+++ ODT.py
@@ -1,19 +1,12 @@
 import zipfile
 
-def findFile(archive, fileName):
-  for fileInfo in archive.filelist:
-    if fileInfo.filename == fileName:
-      return fileInfo
-  return None
-
 class ODT:
   def __init__(self, fileName):
     self._archive = zipfile.ZipFile(fileName)
-    self._contentXML = self._archive.open(findFile(self._archive, "content.xml")).readlines()
+    self._contentXML = self._archive.open("content.xml").read()
   
   def close(self):
     self._archive.close()
-
 
   def getContent(self):
     return self._contentXML
